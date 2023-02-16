3363aab458f8c9f5c23f7a98b3f9b6bbe909fe82
Юлия Викторовна <julsbi@bk.ru>
2023-02-16 18:31:06 +0300

    Added Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа,которая на вход принимает два числа 
+// и выдает, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите число 1:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a==b)
+{
+   Console.WriteLine($"Число {a} равно {b}");
+}
+else
+{
+    if (a > b)
+    {
+        Console.WriteLine($"Максимальное число {a}, Минимальное {b}");
+    }
+    else
+    {
+        Console.WriteLine($" Максимальное число {b}, Минимальное {a}");
+    }
+}
+
