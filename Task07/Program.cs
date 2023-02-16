92765c6197a913e80d4d1fb8e0b32ddf246048f3
Юлия Викторовна <julsbi@bk.ru>
2023-02-16 15:53:15 +0300

    Added Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа, которая принимает на вход 3х зн. число и на выходе показывает последнюю цифру
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+    int lastNumber = number % 10;
+    Console.Write($"Последняя цифра числа - {lastNumber}");
+}
+else Console.WriteLine ("Вы ввели не трехзначное число!");
