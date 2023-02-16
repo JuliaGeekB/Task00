b60d8f0f792bffb909dcb0d289b1c5a1bd944508
Юлия Викторовна <julsbi@bk.ru>
2023-02-16 19:55:37 +0300

    Added Task08

---
+++ Task06/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая на вход принимает число 
+// и выдает, является ли число четным
+Console.WriteLine("введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+b=a%2*2;
+
+if (b == a)
+{
+       Console.WriteLine($"Число {a} четное");
+}
+else
+{
+    Console.WriteLine($"Число {a} нечетное");
+}
