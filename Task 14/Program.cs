cb5a4e9e326f95e6eed68842ab5d39c6f0d1879f
Юлия Викторовна <julsbi@bk.ru>
2023-02-21 12:45:35 +0300

    Added Task 14 and Task 16

---
+++ Task 14/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая принимает на вход число 
+// и проверяет, кратно оно одновременно 7 и 23 
+
+
+Console.WriteLine("Введите число:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool MultiplicityTwoNumbers(int num)
+
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+if (MultiplicityTwoNumbers(number)) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+
---
+++ Task16/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает на вход 2 числа 
+//и проверяет, является ли одно число квадратом другого
+
+Console.WriteLine("Введите число 1:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (Square(firstNumber, secondNumber)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
+
+bool Square(int num1, int num2)
+
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+}
