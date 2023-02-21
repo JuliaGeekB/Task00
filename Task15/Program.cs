d4afdbba2abe15af819124d530a83df7b3e43cc0
Юлия Викторовна <julsbi@bk.ru>
2023-02-21 18:14:17 +0300

    Added Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите номер дня недели:");
+string day = Console.ReadLine();
+if (day == "1") Console.Write("Да");
+else if (day == "2") Console.Write("Да");
+else if (day == "3") Console.Write("Да");
+else if (day == "4") Console.Write("Да");
+else if (day == "5") Console.Write("Да");
+else if (day == "6") Console.Write("Нет");
+else if (day == "7") Console.Write("Нет");
+else Console.Write("Такого дня не существует");
