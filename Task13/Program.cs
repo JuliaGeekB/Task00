f719a01885e47d75188ad91fa41f96f044c70386
Юлия Викторовна <julsbi@bk.ru>
2023-02-21 18:22:46 +0300

    Added Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100)
+{
+    while (number>999)
+    {
+        number=number/10;
+    }
+
+    int thirdDigit = number % 10;
+    Console.Write($"Третья цифра числа - {thirdDigit}");
+}
+else Console.WriteLine("Третьей цифры нет!");
