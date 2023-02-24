53794d539181f0061819e735ebb90253d38883ec
Юлия Викторовна <julsbi@bk.ru>
2023-02-24 18:29:09 +0300

    Added Task 22

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Программа, которая принимает на вход координаты 2х точек и находит расстояние между ними в 2D пространстве.
 
 Console.Write("Введите координаты x точки a: ");
-int xa = Convert.ToInt32 (Console.ReadLine());
+int xa = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координаты y точки a: ");
-int ya = Convert.ToInt32 (Console.ReadLine());
+int ya = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координаты x точки b: ");
-int xb = Convert.ToInt32 (Console.ReadLine());
+int xb = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координаты y точки b: ");
-int yb = Convert.ToInt32 (Console.ReadLine());
+int yb = Convert.ToInt32(Console.ReadLine());
 
-double result = Distance(xa,ya,xb,yb);
-Console.Write(Math.Round(result,2,MidpointRounding.ToZero));
-double Distance(int x1,int y1, int x2, int y2)
+double result = Distance(xa, ya, xb, yb);
+Console.Write(Math.Round(result, 2, MidpointRounding.ToZero));
+double Distance(int x1, int y1, int x2, int y2)
 {
-    double sumSquare=((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+    double sumSquare = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
     double res = Math.Sqrt(sumSquare);
     return res;
 }
---
+++ Task22/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Square(number);
+
+void Square(int num)
+{
+    int index = 1;
+    while (num >= index)
+    {
+        Console.WriteLine($"{index}-{index * index}");
+        index++;
+    }
+}
