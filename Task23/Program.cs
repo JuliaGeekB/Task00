5d62288a75efc08afa3303eadceadb7811646435
Юлия Викторовна <julsbi@bk.ru>
2023-02-24 19:10:48 +0300

    Added Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает на вход число (N) 
+// и выдает таблицу кубов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Cube(number);
+
+void Cube(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index,5}-{index * index * index,5}");
+        index++;
+    }
+}
