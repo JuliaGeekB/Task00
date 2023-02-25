21d80f8df820673c3dcc2cda131080b6ba68afc6
Юлия Викторовна <julsbi@bk.ru>
2023-02-26 01:08:28 +0300

    Added Task 26

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа, которая принмает на вход число 
+// и выдает количество цифр в числе
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = CheckDigit(number);
+Console.WriteLine($"В числе {number}, {digit}");
+
+int CheckDigit(int num)
+{
+    int result = 0;
+    if (num == 0) return 1;
+    else
+    {
+        while (num != 0)
+        {
+            num = num / 10;
+            result = result + 1;
+        }
+    }
+    return result;
+}
