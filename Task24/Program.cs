749ce5825d6262b03a61d2bc7003d252a93cd3d0
Юлия Викторовна <julsbi@bk.ru>
2023-02-26 00:47:28 +0300

    Added Task 24

---
+++ Task24/Program.cs
@@ -0,0 +1,19 @@
+﻿//Программа, которая принмает на вход число (А)
+// и выдает сумму чисел от 1 до А
+
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = SumNumbers(number);
+Console.WriteLine($"Сумма числе от 1 до {number} = {sum}");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+        checked
+        {
+            sum = sum + i;
+        }
+    return sum;
+}
