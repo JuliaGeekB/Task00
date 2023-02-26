c54fe9174b557eb9164d1e65bdaad9b7c6b64b05
Юлия Викторовна <julsbi@bk.ru>
2023-02-27 00:21:54 +0300

    Added Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Цикл, который принмает на вход два числа (А и В) 
+// и возводит число А в натуральную степень В.
+
+Console.WriteLine("Введите целое положительное число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 0 && numberB > 0)
+{
+    int exponentiation = Exponentiation(numberA, numberB);
+    Console.WriteLine($"{exponentiation}");
+}
+else Console.WriteLine("Ошибка ввода. Введены не целые положительные числа.");
+
+int Exponentiation(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+        checked
+        {
+            result = result * numA;
+        }
+    return result;
+}
