b63a4b8b73352e6d7a9984158a35316d2eab0e71
Юлия Викторовна <julsbi@bk.ru>
2023-02-26 23:08:56 +0300

    Added Task 28

---
+++ Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа, которая на вход принимает число N
+// и выдает произведение чисел от 1 до N
+
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>0)
+{
+int factorial = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {factorial}");
+}
+else Console.WriteLine("Введено не целое положительное число. Повторите попытку.");
+
+int Factorial(int num)
+{
+    int multiply = 1;
+    for (int i = 1; i <= num; i++)
+        checked
+        {
+            multiply = multiply * i;
+        }
+    return multiply;
+}
