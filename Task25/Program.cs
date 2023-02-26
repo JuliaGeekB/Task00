be503674f673c2ffbc9c1666959e9aa02494f666
Юлия Викторовна <julsbi@bk.ru>
2023-02-27 01:09:25 +0300

    Added Task 25 with corrects

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,17 +1,17 @@
 ﻿// Цикл, который принмает на вход два числа (А и В) 
 // и возводит число А в натуральную степень В.
 
-Console.WriteLine("Введите целое положительное число А: ");
+Console.WriteLine("Введите число А: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите целое положительное число В: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-if (numberA > 0 && numberB > 0)
+if (numberB > 0)
 {
     int exponentiation = Exponentiation(numberA, numberB);
     Console.WriteLine($"{exponentiation}");
 }
-else Console.WriteLine("Ошибка ввода. Введены не целые положительные числа.");
+else Console.WriteLine("Ошибка ввода. Число В не целое положительное число.");
 
 int Exponentiation(int numA, int numB)
 {
---
+++ Task29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Программа, которая задает массив из 8 элементов, 
+// заполненный псевдослучайными числами и выводит их на экран
+
+Console.WriteLine("Сколько элементов в массиве: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = NewRandomArray(number);
+Console.WriteLine($"Полученный массив из {number}элементов");
+PrintArray(array);
+
+int[] NewRandomArray(int num)
+{
+    Random rand = new Random();
+    int[] arr = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        arr[i] = rand.Next();
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+        if (i < arr.Length - 1) Console.Write($",  ");
+    }
+    Console.WriteLine($"]");
+}
