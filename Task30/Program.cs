b58afbde13dde2e46735b717a2507e4eb131cda0
Юлия Викторовна <julsbi@bk.ru>
2023-02-27 00:07:13 +0300

    Added Task 30

---
+++ Task30/Program.cs
@@ -0,0 +1,30 @@
+﻿// Программа, которая выводит массив из 8 элементов, заполненный 0 и 1 в случайном порядке
+
+
+Console.WriteLine("Сколько элементов в массиве: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = NewRandomArray(number, 0, 1);
+Console.WriteLine($"Полученный массив из {number}элементов");
+PrintArray(array);
+
+int[] NewRandomArray(int num, int min, int max)
+{
+    Random rand = new Random();
+    int[] arr = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        arr[i] = rand.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+        if (i < arr.Length - 1) Console.Write($",  ");
+    }
+    Console.WriteLine($"]");
+}
