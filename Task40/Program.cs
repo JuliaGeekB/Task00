08a90f180ffae981026a2a6b8391786145646d14
Юлия Викторовна <julsbi@bk.ru>
2023-03-07 00:22:12 +0300

    Added Task42

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -2,15 +2,15 @@
 // Теорема: каждая сторона треугольника меньше суммы двух других.
 
 Console.WriteLine("Введите три числа: ");
-int number1=Convert.ToInt32(Console.ReadLine());
-int number2=Convert.ToInt32(Console.ReadLine());
-int number3=Convert.ToInt32(Console.ReadLine());
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
 
-bool IsTriangleExists(int num1,int num2,int num3)
+bool IsTriangleExists(int num1, int num2, int num3)
 {
-    return number1+number2>number3 && number2+number3>number1 && number1+number3>number2;
+    return number1 + number2 > number3 && number2 + number3 > number1 && number1 + number3 > number2;
 }
 
-bool result= IsTriangleExists (number1, number2, number3);
+bool result = IsTriangleExists(number1, number2, number3);
 
-Console.WriteLine(result ? "Yes": "No");
+Console.WriteLine(result ? "Yes" : "No");
---
+++ Task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая будет преобразовывать десятичное число в двоичное
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DecToBin(int num)
+{
+    int count = 1;
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + num % 2 * count;
+        num = num / 2;
+        count *= 10;
+    }
+    return result;
+}
+
+int decToBin = DecToBin(number);
+Console.WriteLine($"{number} -> {decToBin}");
