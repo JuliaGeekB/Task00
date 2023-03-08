7512d81d7312a40667216297444648e7f94e017f
Юлия Викторовна <julsbi@bk.ru>
2023-03-09 00:42:00 +0300

    Added Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,49 @@
+﻿// Пользователь вводит с клавиатуры М чисел.
+// Посчитайте, сколько чисел больше 0 ввел пользователь.
+
+// Считать число с консоли
+int Prompt(string message)
+{
+    Console.Write(message); // вывести сообщение
+    string value = Console.ReadLine(); // считывает строку
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+// Ввести массив
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i + 1}-й элемент:");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}]={array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length = Prompt("Введите количество элементов: ");
+int[] array;
+array = InputArray(length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел > 0 - {CountPositiveNumbers(array)}");
