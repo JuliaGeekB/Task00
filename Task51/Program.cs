63391af58e535c171333b489b285741aa4a05c8c
Юлия Викторовна <julsbi@bk.ru>
2023-03-10 00:56:51 +0300

    Added Task51

---
+++ Task51/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3},  ");
+            else Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+int MainDiagonalSumElements(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+    {
+        sum += matrix[i, i];
+    }
+    return sum;
+}
+
+int[,] array2d = CreateMatrixRndInt(6, 6, -10, 10);
+PrintMatrix(array2d);
+int mainDiagonalSumElements = MainDiagonalSumElements(array2d);
+Console.WriteLine($"Сумма элементов главной диагонали = {mainDiagonalSumElements}");
