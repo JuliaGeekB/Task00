0a13c587842766969c5b7fab7529ffa519efd398
Юлия Викторовна <julsbi@bk.ru>
2023-03-13 00:34:37 +0300

    Added Task59

---
+++ Task59/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задайте двумерный массив из целых чисел.
+// Напишите программу, которая удалит строку и столбец, на пересечении которого
+// расположен наименьший элемент массива
+
+// Methods------------------
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3},  ");
+            else Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[] IndexOfMinElem(int[,] matrix)
+{
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] <= matrix[row, column])
+            {
+                row = i;
+                column = j;
+            }
+        }
+    }
+    return new int[] { row, column };
+}
+
+int[,] DeleteRowColumn(int[,] matrix, int[] array)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+    int m = 0, n = 0;
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        if (m == array[0]) m++;
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            if (n == array[1]) n++;
+            newMatrix[i, j] = matrix[m, n];
+            n++;
+        }
+        m++;
+        n = 0;
+    }
+    return newMatrix;
+}
+
+// Program body------------------
+int[,] array2d = CreateMatrixRndInt(3, 3, 0, 10);
+PrintMatrix(array2d);
+int[] indexOfMinElem = IndexOfMinElem(array2d);
+int[,] newarray2d = DeleteRowColumn(array2d, indexOfMinElem);
+PrintMatrix(newarray2d);
