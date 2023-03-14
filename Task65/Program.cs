9e71a62ab47782783956273675c823a8c32cc39f
Юлия Викторовна <julsbi@bk.ru>
2023-03-15 01:36:26 +0300

    Added Task65

---
+++ Task65/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке между заданными числами.
+
+Console.WriteLine("Введите натуральное число M: ");
+int numberOne = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N: ");
+int numberTwo = int.Parse(Console.ReadLine());
+PrintNumbers(numberOne, numberTwo);
+
+void PrintNumbers(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        Console.Write($"{num1}  ");
+        PrintNumbers(num1 - 1, num2);
+
+    }
+    else if (num2 > num1)
+    {
+
+        PrintNumbers(num1, num2 - 1);
+        Console.Write($"{num2}  ");
+    }
+    else
+    {
+        Console.Write($"{num2}  ");
+    }
+}
