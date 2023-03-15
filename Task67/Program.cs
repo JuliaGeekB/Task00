42b9126b3e0e08cb958fe22006e8e59437c26eee
Юлия Викторовна <julsbi@bk.ru>
2023-03-16 01:00:33 +0300

    Added Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, которая юудет принимать на вход и возвращать сумму его цифр.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    if (num == 0) return 0;
+    else return num % 10 + SumDigits(num / 10);
+}
+
+Console.WriteLine($"{SumDigits(number)}");
